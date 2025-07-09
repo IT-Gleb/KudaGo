b99e90bcedb812c0b605982042cef555549a6dc0
IT-Gleb <139981680+IT-Gleb@users.noreply.github.com>
2025-07-09 08:27:02 +0300

    fix:mobile video size update

--- components/ui/film/filmComponent.vue
+++ components/ui/film/filmComponent.vue
@@ -1,16 +1,32 @@
 <script setup lang="ts">
+import { nanoid } from "nanoid";
+
+type TFilmObject = {
+  id: string;
+  name: string;
+  link: string;
+};
+
 const shablon: string = "https://rutube.ru/play/embed/%s";
-const ids: string[] = ["80d9d631ec25cfd5f2c854025e1947a8"];
+const ids: TFilmObject[] = [
+  {
+    id: nanoid(),
+    name: "Отпетые мошенники",
+    link: "80d9d631ec25cfd5f2c854025e1947a8",
+  },
+];
 
 const filmUrl = ref<string>("");
 
 const reg = new RegExp("%s", "gmi");
 
-filmUrl.value = shablon.replace(reg, ids[0]);
+filmUrl.value = shablon.replace(reg, ids[0].link);
 </script>
 
 <template>
-  <div class="my-10 aspect-video w-fit mx-auto object-cover object-center">
+  <div
+    class="my-10 w-[320px] h-[212px] sm:w-[420px] sm:h-[280px] lg:w-[720px] lg:h-[405px] aspect-video mx-auto object-cover object-center"
+  >
     <iframe
       width="720"
       height="405"
@@ -18,6 +34,10 @@ filmUrl.value = shablon.replace(reg, ids[0]);
       frameBorder="0"
       allow="clipboard-write; autoplay"
       allowFullScreen
+      class="block w-full h-full"
     ></iframe>
   </div>
 </template>
+
+<!-- width="720"
+      height="405" -->
